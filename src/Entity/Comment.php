8e2ef0075c4f3363a9142093d20800252685decc
Pavel Kondybailo <pkondybailo@aristeksystems.com>
2022-10-17 21:19:32 +0300

    feat: add admin panel

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -33,6 +33,11 @@ class Comment
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $photoFilename = null;
 
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
