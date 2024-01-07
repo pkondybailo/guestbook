2f11c967f01cc84ae6df32d782a623362cbc39cc
Pavel Kondybailo <pkondybailo@aristeksystems.com>
2024-01-07 20:17:47 +0300

    feat: add cronjob to cleanup old rejected and spam comments

--- src/Enum/CommentStatusEnum.php
+++ src/Enum/CommentStatusEnum.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum CommentStatusEnum: string
 {
     case Published = 'published';
+    case Rejected = 'rejected';
     case Spam = 'spam';
     case Submitted = 'submitted';
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -5,7 +5,9 @@ namespace App\Repository;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Enum\CommentStatusEnum;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -20,6 +22,7 @@ use Doctrine\Persistence\ManagerRegistry;
 class CommentRepository extends ServiceEntityRepository
 {
     public const PAGINATOR_PER_PAGE = 2;
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
 
     public function __construct(ManagerRegistry $registry)
     {
@@ -35,13 +38,22 @@ class CommentRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Comment $entity, bool $flush = false): void
+    public function countOldRejected(): int
     {
-        $this->getEntityManager()->remove($entity);
+        return $this
+            ->getOldRejectedQueryBuilder()
+            ->select('COUNT(c.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+    public function deleteOldRejected(): int
+    {
+        return $this
+            ->getOldRejectedQueryBuilder()
+            ->delete()
+            ->getQuery()
+            ->execute();
     }
 
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
@@ -57,5 +69,27 @@ class CommentRepository extends ServiceEntityRepository
             ->getQuery();
 
         return new Paginator($query);
+    }
+
+    public function remove(Comment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this
+            ->createQueryBuilder('c')
+            ->andWhere('c.status = :status_rejected or c.status = :status_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'status_rejected' => CommentStatusEnum::Rejected->value,
+                'status_spam'     => CommentStatusEnum::Spam->value,
+                'date'            => new DateTimeImmutable(-self::DAYS_BEFORE_REJECTED_REMOVAL.' days'),
+            ]);
     }
 }
